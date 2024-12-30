fc7b78d79028490e381b6f90286153962439285f
Kozimjon <kozimjonmalikov0727@gmail.com>
2024-12-30 18:05:37 +0500

    user added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Posts from './components/posts/Posts'
+import User from './components/users/User'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route element={<Posts />} path='/' ></Route>
+        <Route element={<User />} path='/' ></Route>
       </Routes>
     </BrowserRouter>
   )
--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function User() {
     return (
         <div>
-            Users
+            Users new
         </div>
     )
 }
