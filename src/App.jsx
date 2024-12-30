bf14e5f21aed5a47d17a8a7bba20abfd01a3a123
Kozimjon <kozimjonmalikov0727@gmail.com>
2024-12-30 18:47:34 +0500

    test -3

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Posts from './components/posts/Posts'
-import User from './components/users/User'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route element={<User />} path='/' ></Route>
+        <Route element={<Posts />} path='/' ></Route>
       </Routes>
     </BrowserRouter>
   )
